d4591acb28ffe77bcc478462af83176ee866dc2f
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2023-09-01 17:44:56 +0200

    spellingmistake

--- WebApplicationEFDEmo/Program.cs
+++ WebApplicationEFDEmo/Program.cs
@@ -9,10 +9,7 @@ namespace WebApplicationEFDEmo
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -23,7 +20,6 @@ namespace WebApplicationEFDEmo
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
